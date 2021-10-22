6115f809f8e6721e889359780680ff50feab78b3
Zephyr Astra <this.zephyr@gmail.com>
2021-10-22 13:43:00 -0700

    send and read message option added

--- components/WebConnection.js
+++ components/WebConnection.js
@@ -18,6 +18,8 @@ const pc = new RTCPeerConnection(configuration);
 const WebConnection = () => {
 
     const [description, setDescription] = useState();
+    const [message, setMessage] = useState('no message');
+    const [sMessage, setSMessage] = useState('no Message');
 
     const setRemoteDescription = async () => {
         const remoteDesc = new RTCSessionDescription(JSON.parse(description));
@@ -27,8 +29,6 @@ const WebConnection = () => {
                 pc.setLocalDescription(desc).then(()=> {
                     //generate answer
                     console.log(JSON.stringify(pc.localDescription));
-                    console.log("other");
-                    console.log(desc);
                 })
             });
             });
@@ -38,21 +38,56 @@ const WebConnection = () => {
         setRemoteDescription();
     }
 
-    pc.addEventListener('connectionstatechange', event => {
+    pc.addEventListener('connectionstatechange', e => {
         if (pc.connectionState === 'connected') {
-            console.log("connected")
+            console.log("connected");
+            console.log(e);
         }
     });
+
+    const dataChannel = pc.createDataChannel('channel');
+
+    pc.addEventListener('datachannel', e => {
+        pc.dataChannel = e.channel;
+
+        pc.dataChannel.addEventListener('message', e => {
+            console.log(e.data);
+            setMessage(e.data);
+        })
+    })
+
+    const sendMessage = () =>{
+        console.log('sending');
+        dataChannel.send(sMessage);
+    }
+
+
 
     return(
     <View>
     <Text>WebRTC</Text>
+    
+    {/* offer from remote */}
     <TextInput style={styles.input}
     placeholder = "Enter your offer"
     onChangeText = {setDescription}
     />
     <TouchableOpacity onPress={submitButton}>
-        <Text>Generate Answer</Text>
+        <Text style={styles.button} >Generate Answer</Text>
+    </TouchableOpacity>
+    
+    {/* messages from remote */}
+    <Text>Message from remote</Text>
+    <Text>{message}</Text>
+
+    {/* Send New Message  */}
+    <Text>Send Message</Text>
+    <TextInput style={styles.input}
+    placeholder = "new message"
+    onChangeText = {setSMessage}
+    />
+    <TouchableOpacity onPress={sendMessage}>
+        <Text style={styles.button}>Send</Text>
     </TouchableOpacity>
     </View>
     )
@@ -64,6 +99,9 @@ const styles = StyleSheet.create({
       margin: 12,
       borderWidth: 1,
       padding: 10,
+    },
+    button: {
+        fontSize: 20,
     },
 })
 
