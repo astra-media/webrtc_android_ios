8b78811e6526b9cb1ee474e9a30c93a8df367c8a
Zephyr Astra <this.zephyr@gmail.com>
2021-10-21 17:30:25 -0700

    function for webrtc connection

--- components/WebConnection.js
+++ components/WebConnection.js
@@ -1,23 +1,59 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
 
-const WebConnection = () => {
-    const [text, setText] = useState('');
+import {
+    RTCPeerConnection,
+    RTCIceCandidate,
+    RTCSessionDescription,
+    RTCView,
+    MediaStream,
+    MediaStreamTrack,
+    mediaDevices,
+    registerGlobals,
+  } from 'react-native-webrtc';
 
-    const submitButton = () =>{
-        console.log(text)
+const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+const pc = new RTCPeerConnection(configuration);
+
+const WebConnection = () => {
+
+    const [description, setDescription] = useState();
+
+    const setRemoteDescription = async () => {
+        const remoteDesc = new RTCSessionDescription(JSON.parse(description));
+        pc.setRemoteDescription(remoteDesc).then(() => {
+            // console.log(JSON.stringify(pc.remoteDescription));
+            pc.createAnswer().then(desc => {
+                pc.setLocalDescription(desc).then(()=> {
+                    //generate answer
+                    console.log(JSON.stringify(pc.localDescription));
+                    console.log("other");
+                    console.log(desc);
+                })
+            });
+            });
     }
+
+    const submitButton = async () =>{
+        setRemoteDescription();
+    }
+
+    pc.addEventListener('connectionstatechange', event => {
+        if (pc.connectionState === 'connected') {
+            console.log("connected")
+        }
+    });
 
     return(
     <View>
     <Text>WebRTC</Text>
     <TextInput style={styles.input}
     placeholder = "Enter your offer"
-    onChangeText = {setText}
+    onChangeText = {setDescription}
     />
     <TouchableOpacity onPress={submitButton}>
         <Text>Generate Answer</Text>
-        </TouchableOpacity>
+    </TouchableOpacity>
     </View>
     )
 }
